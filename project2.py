fd8a527a1ca5836697fb32202d3af052ae53de56
williamdalessi <36385898+williamdalessi@users.noreply.github.com>
2020-04-20 21:27:51 -0700

    Update project2.py
    Updated based on changes

--- project2.py
+++ project2.py
@@ -1,9 +1,3 @@
-'''Trains a simple deep NN on the MNIST dataset.
-Gets to 98.40% test accuracy after 20 epochs
-(there is *a lot* of margin for parameter tuning).
-2 seconds per epoch on a K520 GPU.
-'''
-
 from __future__ import print_function
 
 import tensorflow as tf
@@ -12,26 +6,18 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Dropout
 from tensorflow.keras.optimizers import RMSprop
 
-'''Importing the EMNIST letters'''
+# Importing the EMNIST letters
 from scipy import io as sio
 
+
+# Batch size of 128 had about 90.4% accuracy.
+# Thus, a batch size of 1000 was used where accuracy was about 91.5%. 
+# Signifigantly higher batch sizes also decreased test accuracy.
+# A batch size of 104,000 led to an accuracy of about 
 batch_size = 1000
 # num_classes = 10
 num_classes = 26
-epochs = 1000
-
-# the data, split between train and test sets
-# (x_train, y_train), (x_test, y_test) = mnist.load_data()
-
-# x_train = x_train.reshape(60000, 784)
-# x_test = x_test.reshape(10000, 784)
-# x_train = x_train.astype('float32')
-# x_test = x_test.astype('float32')
-# x_train /= 255
-# x_test /= 255
-# print(x_train.shape[0], 'train samples')
-# print(x_test.shape[0], 'test samples')
-
+epochs = 1000 #There is early stopping, so it won't reach 1000 epochs. This needs to be high.
 
 # https://stackoverflow.com/questions/51125969/loading-emnist-letters-dataset/53547262#53547262
 mat = sio.loadmat('emnist-letters.mat')
@@ -56,9 +42,14 @@ y_test = tf.keras.utils.to_categorical(y_test - 1, num_classes, dtype='float32')
 y_val = tf.keras.utils.to_categorical(y_val - 1, num_classes, dtype='float32')
 
 model = Sequential()
-model.add(Dense(512, activation='tanh', input_shape=(784,)))
+# Sigmoid seemed to work better for test accuracy compared to relu. (sigmoid was getting 91% test accuracy compared to 89% for relu.)
+# Sigmoid was slighly better than tanh, but both were about the same test accuracy (within a few tenths of a percent)
+model.add(Dense(512, activation='sigmoid', input_shape=(784,)))
+# Tried different dropout rates, but 0.2 seemed to work well and provided a modest improvement.
+# (~0.5% test accuracy improvement compared to not using dropout at all)
 model.add(Dropout(0.2))
-model.add(Dense(512, activation='tanh'))
+# Compared to other numbers of neurons, this number seemed to work well (2000 hidden neurons)
+model.add(Dense(2000, activation='sigmoid'))
 model.add(Dropout(0.2))
 model.add(Dense(num_classes, activation='softmax'))
 
@@ -71,14 +62,13 @@ model.compile(loss='categorical_crossentropy',
 
 # https://www.tensorflow.org/api_docs/python/tf/keras/callbacks/EarlyStopping
 earlyStop = tf.keras.callbacks.EarlyStopping(
-    monitor='val_loss', min_delta=0.001, patience=10, verbose=0, mode='auto',
-    baseline=None, restore_best_weights=False
+    monitor='val_loss', min_delta=0.0001, patience=5, verbose=0, mode='auto',
+    baseline=None, restore_best_weights=True
 )
 
 history = model.fit(x_train, y_train,
                     batch_size=batch_size,
                     epochs=epochs, callbacks=[earlyStop],
-                    verbose=1,
                     validation_data=(x_val, y_val)
                     )
 score = model.evaluate(x_test, y_test, verbose=0)
